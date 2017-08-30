72bbb1889181b71caa4684fcaf81fdd283e14c26
Shu <wangshu214@live.cn>
2017-08-30 23:44:53 +0800

    Daily work for 2017-08-30

--- Algorithms In CSharp/Chapter1/BasicStaticMethodImpl.cs
+++ Algorithms In CSharp/Chapter1/BasicStaticMethodImpl.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Drawing;
 using System.Windows.Forms;
 using System.Collections;
-
+using System.Diagnostics;
 namespace Algorithms_In_CSharp.Chapter1
 {
     //静态方法实现
@@ -366,5 +366,352 @@ namespace Algorithms_In_CSharp.Chapter1
             }
         }
 
+    }
+    //提高题1.3.33 双向链表实现双向队列
+    namespace DequeDemo
+    {
+        class Deque<Item>:IEnumerable<Item>
+        {
+            class Node
+            {
+                public Node prior =null;//前驱结点
+                public Node next =null;//后继节点
+                public Item Data { get; set; }
+            }
+            //迭代器:从左往右
+            class Iterator : IEnumerator<Item>
+            {
+                //定义表头表示结束
+                public Node head;
+                public Node current;
+                //构造函数
+                public Iterator(Node head)
+                {
+                    this.head = head;
+                    this.current = head;
+                }
+                public Item Current => current.Data;
+                object IEnumerator.Current => current.Data;
+
+                public void Dispose()
+                {
+                    //throw new NotImplementedException();
+                }
+
+                public bool MoveNext()
+                {
+                    current = current.next;
+                    return current != head;
+                }
+
+                public void Reset()
+                {
+                    head = null;
+                    current = null;
+                }
+            }
+            //表头
+            Node head;
+            //是否为空
+            public bool IsEmpty()
+            {
+                return currentSize == 0;
+            }
+            //构造函数
+            public Deque()
+            {
+                this.head = new Node();
+                //设置初始状态
+                this.head.next = this.head;
+                this.head.prior = this.head;
+            }
+            //当前的大小
+            int currentSize = 0;
+            //左入队
+            public void PushLeft(Item item)
+            {
+                //注意替换表头
+                Node newNode = new Node();
+                newNode.Data = item;
+                if(IsEmpty())
+                {
+                    newNode.next = this.head;
+                    newNode.prior = this.head;
+                    this.head.prior = newNode;
+                    this.head.next = newNode;
+                }
+                else
+                {
+                    newNode.next = this.head.next;
+                    newNode.prior = this.head;
+                    this.head.next.prior = newNode;
+                    this.head.next = newNode;
+                }
+                
+                ++currentSize;
+            }
+            //右入队
+            public void PushRight(Item item)
+            {
+                Node newNode = new Node();
+                newNode.Data = item;
+                newNode.prior = this.head.prior;
+                newNode.next = this.head;
+                this.head.prior.next = newNode;
+                this.head.prior = newNode;
+                ++currentSize;
+            }
+            //左出队
+            public Item PopLeft()
+            {
+                if (!IsEmpty())
+                {
+                    Item left = this.head.next.Data;
+                    this.head.next.next.prior = this.head;
+                    this.head.next = this.head.next.next;
+                    --currentSize;
+                    return left;
+                }
+                throw new Exception("没有元素可以左出队");
+            }
+            //右出队
+            public Item PopRight()
+            {
+                if (!IsEmpty())
+                {
+                    Item right = this.head.prior.Data;
+                    this.head.prior.prior.next = this.head;
+                    this.head.prior = this.head.prior.prior;
+                    
+                    --currentSize;
+                    return right;
+                }
+                throw new Exception("没有元素可以右出队");
+            }
+
+            public IEnumerator<Item> GetEnumerator()
+            {
+                return new Iterator(head);
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return new Iterator(head);
+            }
+        }
+        class App
+        {
+            static void Main()
+            {
+                Deque<int> deque = new Deque<int>();
+                Deque<int> empty = new Deque<int>();
+                foreach (int i in Enumerable.Range(1, 10))
+                    deque.PushRight(i);
+                //foreach (int i in Enumerable.Range(1, 10))
+                //    Console.WriteLine(deque.PopRight());
+                foreach(var item in deque)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+    //提高题1.3.39 环形缓冲区 https://zh.wikipedia.org/wiki/%E7%92%B0%E5%BD%A2%E7%B7%A9%E8%A1%9D%E5%8D%80
+    //full的判断条件 R-H+1==0(R H是队尾和对头)
+    //1.4算法分析
+    namespace AlgorithmAnalysisDemo
+    {
+        //ref http://lxy.me/c-code-run-time.html
+        using System.Threading;
+        using System.Diagnostics;//引用相关的命名空间
+        using System.Runtime.InteropServices;
+
+        //计数器类
+        static class Timer
+        {
+            /// <summary>
+            /// 支持action类型（返回调用时间。单位ms）
+            /// </summary>
+            /// <param name="action">Action类型</param>
+            /// <returns></returns>
+            public static long Count(Action action)
+            {
+                Stopwatch Stopwatch = new Stopwatch();
+                Stopwatch.Start();
+                action();
+                Stopwatch.Stop();
+                return Stopwatch.ElapsedMilliseconds;
+            }
+
+        }
+        class App
+        {
+            [StructLayout(LayoutKind.Sequential)]
+            class I
+            {
+                int i;
+            }
+            //方法例子：
+            public static void Sleep()
+            {
+                Thread.Sleep(3000);
+            }
+            public static void Main()
+            {
+                Console.WriteLine(Timer.Count(Sleep));
+                Console.ReadKey();
+            }
+        }
+    }
+    //算法1.5 并查集
+    namespace UnionFindDemo
+    {
+        class UnionFind
+        {
+            //保存所有触点的一维数组
+            private int[] id;
+            //各个触点所对应的连通分量的大小
+            private int[] sz;
+            //保存触点的大小
+            private int count;
+            public UnionFind(int size)
+            {
+                id = new int[size];
+                sz = new int[size];
+                for (int i = 0; i < size; ++i)
+                {
+                    id[i] = i;
+                    sz[i] = 1;//自身连接的分量大小
+                }
+                count = size;
+            }
+            //将p、q连接
+            public void Union(int p,int q)
+            {
+                int pID = Find(p);
+                int qID = Find(q);
+                if (pID == qID) return;
+                for (int i = 0; i < id.Length; ++i)
+                    if (id[i] == pID)
+                        id[i] = qID;
+                count--;
+            }
+            //获取p所在的分量
+            public int Find(int p)
+            {
+                return id[p];
+            }
+            //判断p、q是否连接
+            public bool IsConnected(int p,int q)
+            {
+                return Find(p) == Find(q);
+            }
+            public int Count()
+            {
+                return count;
+            }
+            //快速
+            public int Find_QuickUnion(int p)
+            {
+                while (p != id[p])
+                    p = id[p];
+                return p;
+            }
+            public void Union_QuickUnion(int p,int q)
+            {
+                int pRoot = Find_QuickUnion(p);
+                int qRoot = Find_QuickUnion(q);
+                if(pRoot !=qRoot)
+                {
+                    id[pRoot] = qRoot;//将q的子树转移到右子树
+                    --count;
+                }
+            }
+            //归并树(加权)
+            public void Union_UnionSubTree(int p,int q)
+            {
+                int pRoot = Find_CompressPath(p);
+                int qRoot = Find_CompressPath(q);//使用路径压缩算法
+                if(pRoot !=qRoot)
+                {
+                    if(sz[pRoot] > sz[qRoot])
+                    {
+                        id[qRoot] = pRoot;
+                        sz[pRoot] += sz[qRoot];
+                        
+                    }
+                    else
+                    {
+                        id[pRoot] = qRoot;
+                        sz[qRoot] += sz[pRoot];
+                    }
+                    --count;
+                }
+            }
+            //Find路径压缩
+            public int Find_CompressPath(int p)
+            {
+                if(id[p] ==p)
+                {
+                    //基本情况 如果已经找到了根节点，就设置然后返回根节点，赋值给路径上的点
+                    return p;
+                }
+                return id[p]=Find_CompressPath(id[p]);
+            }
+        }
+        class App
+        {
+            //实现1.5.2.1
+            static void Main()
+            {
+                int totalIDs = 50000000;
+                int unions = 200000;
+                int offset = unions / 2;
+                UnionFind uf = new UnionFind(totalIDs);
+                UnionFind uf_quick = new UnionFind(totalIDs);//使用快速连接
+                UnionFind uf_quick_compress = new UnionFind(totalIDs);//使用路径压缩和快速连接
+                //uf.Union(0, 1);
+                //uf.Union(0, 5);
+                //uf.Union(2, 6);
+                //uf.Union(6, 7);
+                //uf.Union(3, 8);
+                //uf.Union(3,9);
+                //uf.Union(4, 3);
+                //uf.Union_UnionSubTree(0, 1);
+                //uf.Union_UnionSubTree(0, 5);
+                //uf.Union_UnionSubTree(2, 6);
+                //uf.Union_UnionSubTree(6, 7);
+                //uf.Union_UnionSubTree(3, 8);
+                //uf.Union_UnionSubTree(3, 9);
+                //uf.Union_UnionSubTree(4, 3);
+                int[] links = Common.GenerateRadomData(unions,0, totalIDs);
+                Stopwatch Stopwatch1 = new Stopwatch();
+                Stopwatch1.Start();
+                for (int i = 0; i < offset; ++i)
+                    ;//uf.Union(links[i], links[i + offset]);
+                Stopwatch1.Stop();
+                Console.WriteLine("没有优化Use :" + Stopwatch1.ElapsedMilliseconds.ToString());
+                Console.WriteLine(uf.Count());
+                //quick-union
+                Stopwatch Stopwatch2 = new Stopwatch();
+                Stopwatch2.Start();
+                for (int i = 0; i < offset; ++i)
+                    uf_quick.Union_QuickUnion(links[i], links[i + offset]);
+                Stopwatch2.Stop();
+                Console.WriteLine("快速union Use :" + Stopwatch2.ElapsedMilliseconds.ToString());
+                Console.WriteLine(uf_quick.Count());
+                //quick-union和压缩路径
+                Stopwatch Stopwatch3 = new Stopwatch();
+                Stopwatch3.Start();
+                for (int i = 0; i < offset; ++i)
+                    uf_quick_compress.Union_UnionSubTree(links[i], links[i + offset]);
+                Stopwatch3.Stop();
+                Console.WriteLine("快速union+路径压缩 use :" + Stopwatch3.ElapsedMilliseconds.ToString());
+                Console.WriteLine(uf_quick_compress.Count());
+                Console.ReadKey();
+            }
+            
+        }
     }
 }
---
+++ Algorithms In CSharp/Chapter2/Sort.cs
@@ -0,0 +1,158 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms_In_CSharp.Chapter2
+{
+    namespace  SortDemos
+    {
+        //扩展方法：显示数组元素
+        public static class Extensions
+        {
+            public static void Show<T>(this T[] a)
+            {
+                for (int i = 0; i < a.Length; ++i)
+                    Console.Write(a[i] + " ");
+                Console.Write("\n");
+            }
+        }
+        //所有比较的模板类
+        class SortTemplate
+        {
+            //排序的接口
+            public static void Sort<T>(T[] a) { }
+            //less方法用于对元素的比较
+            public static bool Less<T>(T v, T w)
+                where T:IComparable<T>
+            {
+                return v.CompareTo(w) < 0;
+            }
+            //exchange方法交换元素的位置
+            public static void Exchange<T>(T[] a,int i,int j)
+            {
+                T temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+            //打印数组
+            public static void Show<T>(T[] a)
+            {
+                for(int i=0;i<a.Length;++i)
+                {
+                    Console.Write(a[i] + " ");
+
+                }
+                Console.Write("\n");
+            }
+            //数组是否有序
+            public static bool IsSorted<T>(T[] a)
+                where T : IComparable<T>
+            {
+                for (int i = 1; i < a.Length; ++i)
+                    if (Less(a[i], a[i - 1]))
+                        return false;
+                return true;
+            }
+        }
+        //希尔排序
+        class Shell: SortTemplate
+        {
+            //覆盖
+            public new static void Sort<T>(T[] a)
+                where T : IComparable<T>
+            {
+                int N = a.Length;
+                int h = 1;
+                while (h < N / 3)
+                    h = 3 * h + 1;//选择间隔。这个间隔没有固定的值
+                while(h>=1)
+                {
+                    for(int i=h;i<N;++i)
+                    {
+                        for(int j =i;j>=h && Less(a[j],a[j-h]);j-=h)
+                        {
+                            Exchange(a, j, j - h);
+                        }
+                        //一组以h为间隔的数组排序完成
+                    }
+                    h = h / 3;
+                }
+            }
+        }
+        //归并排序
+        class Merge:SortTemplate
+        {
+            private static int[] aux;//辅助数组
+            //假设数组a[lo,...,mid]和a[mid+1,hi]已经是有序的，那么就需要将两个元素按照从大到小的顺序选择放入目标数组a
+            private static void DoMerge(int[] a,int lo,int mid,int hi)
+            {
+                int i = lo;
+                int j = mid + 1;
+                for (int k = lo; k <= hi; ++k)
+                    aux[k] = a[k];//拷贝需要划分的数组到辅助数组
+                for(int k=lo;k<=hi;++k)
+                {
+                    if (i > mid)
+                        a[k] = aux[j++];//如果第一个数组不足了，就从右边选取元素（这个数组已经排过序了，且第一个数组至少小于第二个数组的第一个没有使用的元素）
+                    else if (j > hi)
+                        a[k] = aux[i++];//同上
+                    else if (Less(aux[j], aux[i]))
+                        a[k] = aux[j++];
+                    else
+                        a[k] = aux[i++];
+                }
+            }
+            //将数组a[lo,...,hi]合并
+            private static void Sort(int[] a,int lo,int hi)
+            {
+                if (hi <= lo)
+                    return;//基本情况，已经将数组划分为连续的两个元素，并且排了序
+                int mid = (hi - lo) / 2 + lo;
+                Sort(a, lo, mid);
+                Sort(a, mid + 1, hi);
+                //假设两段子数组a[lo...mid]和[mid+1...hi]已经排序了
+                if (!Less(a[mid+1], a[mid]))
+                    return;//如果已经是有序（第一个数组的最大<=第二个最小使用!Less可以判断）
+                DoMerge(a, lo, mid, hi);
+            }
+            public static void Sort(int[] a)
+            {
+                aux = new int[a.Length];
+                Sort(a, 0, a.Length - 1);
+            }
+            //自底向上归并排序：划分子数组，然后两个两个合并
+            public static void SortBU(int[] a)
+            {
+                int N = a.Length;
+                aux = new int[N];
+                for (int sz = 1; sz < N; sz = sz + sz)
+                {
+                    //每个子数组的元素个数为2^sz
+                    for (int lo = 0; lo < N - sz/*最后一个数组的其实位置*/; lo += sz + sz/*跳转到当前长度的下一组*/)
+                    {
+                        DoMerge(a, lo, lo + sz - 1, Math.Min(lo + sz + sz - 1, N - 1)/*如果N不是2^N，那么可能会溢出*/);
+                    }
+                }
+            }
+        }
+        //测试程序
+        class App
+        {
+            static void Main()
+            {
+                int[] a = Common.GenerateRadomData(10000, 0, 5000);
+                a.Show();
+                //Shell.Sort(a);
+                //Console.WriteLine(Shell.IsSorted(a));
+                Merge.SortBU(a);
+                //Merge.Sort(a);//如果a是有序的，排序只消耗线性事件
+                Console.WriteLine(Merge.IsSorted(a));
+                a.Show();
+                Console.ReadKey();
+            }
+        }
+    }
+}
